68a44d99f46068f44ea4ef74a0e01ede982fee7e
jenwilson521 <jen.wilson521@gmail.com>
2019-12-12 14:24:14 -0800

    extracting preventative pairs

--- Code/get_drug_ints_to_test_in_EHR.py
+++ Code/get_drug_ints_to_test_in_EHR.py
@@ -5,27 +5,19 @@
 import pickle, os, csv
 import pandas as pd
 
+# load data
 f = '../data/cotherapy/supp4_summary_drug_interactions.xlsx'
-
 dfs = pd.read_excel(f, sheetname=None)
+
+# merge all sheets into a single sheet
 sumdf = pd.concat([df.assign(dme=n) for n,df in dfs.items()])
 new_cols = ['Predicted Cotherapy','Relationship','dme','Drugs with labeled DME','DME pathway intermediate','Intermediate node count','PMID','Notes']
-
 sumdf = sumdf[new_cols]
-
 sumdf.to_csv(open('../data/cotherapy/summary_data_for_EHR.csv','w'),index=False)
 
+# specifically select for DME combinations that would exacerbate a DME
 toi = ['Activates','Aggravates','Induces'] # interaction types of interest
-
 actdf = sumdf.loc[sumdf['Relationship'].isin(toi)]
-#actdf.explode('Drugs with labeled DME')
-#
-#pd.DataFrame([
-#    [c, s, e, n]
-#    for c, S, E, n in df.itertuples(index=False)
-#    for s, e in zip(S.split(','), E.split(','))
-#], columns=df.columns)
-
 split_data = []
 for (c,r,d,dlist,pgenes,p,n,ext) in actdf.itertuples(index=False):
 	d_split = list(set([d for a in dlist.split('|') for d in a.split(',')]))
@@ -33,4 +25,15 @@ for (c,r,d,dlist,pgenes,p,n,ext) in actdf.itertuples(index=False):
 		split_data.append([c,r,d,dname,pgenes,p,n,ext])
 actdf_split = pd.DataFrame(split_data, columns = actdf.columns)
 actdf_split.to_csv(open('../data/cotherapy/activate_summary_data_for_EHR.csv','w'),index=False)
+
+# repeat process to look for combinations that prevent a DME
+toi = ['Prevcents','Prevents','Prevents?','Inhibits']
+predf = sumdf.loc[sumdf['Relationship'].isin(toi)]
+split_data = []
+for (c,r,d,dlist,pgenes,p,n,ext) in predf.itertuples(index=False):
+	d_split = list(set([d for a in dlist.split('|') for d in a.split(',')]))
+	for dname in d_split:
+		split_data.append([c,r,d,dname,pgenes,p,n,ext])
+predf_split = pd.DataFrame(split_data, columns = predf.columns)
+predf_split.to_csv(open('../data/cotherapy/prevent_summary_data_for_EHR.csv','w'),index=False) 
 
